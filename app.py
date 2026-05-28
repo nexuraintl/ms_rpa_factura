9c7fb54022ddad70e74f1d3f0ba700452416665c
Mateo Pereira <mpereira@nexura.com.co>
2026-05-28 19:24:36 +0000

    Updated port 8080 in app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ app.mount("/", StaticFiles(directory="frontend/dist", html=True), name="frontend
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
